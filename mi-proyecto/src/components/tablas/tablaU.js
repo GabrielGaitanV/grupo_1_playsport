3ef037e59004e961a5d24fa47dd8227a7b7c6867
GabrielGaitanV <gabo13gabrielo@gmail.com>
2023-05-08 11:13:40 -0500

    tabla de usuarios

--- mi-proyecto/src/components/tablas/tablaU.js
+++ mi-proyecto/src/components/tablas/tablaU.js
@@ -1,11 +1,55 @@
-import React from "react";
+import {Component} from "react";
 
-function Tabla() {
-    return (
-    <div className="table">
-        
-    </div>
-    );
+ class TableU extends Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      tableRowData: [],
+    };
   }
 
-export default Tabla;
+  componentDidMount() {
+    fetch('http://localhost:3007/api/users/')
+      .then((response) => response.json())
+      .then((users) =>{
+        console.log(users);
+        this.setState({
+          tableRowData: users.users
+        });
+      });
+  }
+  render(){
+    return(
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>emial</th>
+            <th>Imagen</th>
+          </tr> 
+        </thead>
+        <tbody>
+
+            {this.state.tableRowData.map((row)=>{
+              return (
+                <tr key={row.first_name}>
+                  <td>{row.first_name}</td>
+                  <td>{row.last_name}</td>
+                  <td>{row.user_email}</td>
+                  <td>
+                   <img 
+                   style={{width: '60%'}}
+                   src={"http://localhost:3007" + row.image}
+                   /> 
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+ export default TableU;
